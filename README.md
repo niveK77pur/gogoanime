61075d68ae6e6b8894e712673523c4351e7a9c4f
Kevin Laurent Biewesch <KevinBiewesch@yahoo.fr>
2022-02-20 16:49:00 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,65 @@
+Extract MP4 video file links from 'www.gogoanime.so'
+(or wheverever this points to)
+
+# Using the script
+
+Please also check the following section "About the implementation".
+
+Usage:
+    ./gogoanime.py <search term>
+
+The links will be saved to a file in a corresponding folder. Downloading should
+be taken care of outside of this program. NOTE: the links file is structured
+for usage with the Aria2 downloading utility.
+
+Example:
+Note that the search term does not need to be quoted. The following would be
+valid and equivalent queries:
+    ./gogoanime.py yahari ore
+    ./gogoanime.py "yahari ore"
+
+
+# About the implementation
+
+Accessing the download page (when pressing the yellow download button on an
+episode on gogoanime) requires you to first complete a CAPTCHA verification.
+Circumventing this verification is achieved by setting the 'User-Agent' and
+the 'cf_clearance' cookie in the corresponding GET requests.
+
+!! THAT SAID YOU NEED TO CHANGE/UPDATE THE COOKIE IF THE CAPTCHA IS TRIGGERED.
+
+The first section in this code, called "Parameters", found after the imports,
+presents two dictionaries that you can and may have to tweak. They are called
+'headers' and 'cookies'.
+
+The CAPTCHA may also be triggered due to something other than invalid cookies
+(i.e. invalid User-Agent).  Please inspect the GET request to the download page
+(yellow button) in your browser and check which parameters and cookies are
+being set. Update the dictionaries at the top of this script accordingly.
+
+You can check this information by using the Developer Tools in your browser
+(F12 in FireFox). Navigate to the "Network" tab and refresh the page (F5). The
+first request should be a GET request to 'streamani.net/download'. Look in the
+Response and Request Headers for Cookies and other relevant information (i.e.
+User-Agent).
+
+
+# About downloading
+
+For downloading with Aria2:
+    Once you have extracted the links with this script, you can run the
+    following command. NOTE: you should be inside the directory where the
+    links are stored.
+
+    # aria2c -i links.txt -c -x12 -s12 -k10M --max-overall-download-limit=3M
+
+    Flags are as follows:
+        -i  --input-file                    Where the URLs are stored
+        -c  --continue                      Resume on partially downloaded files
+        -x  --max-connection-per-server     Default is 1
+        -s  --split                         Number of parallel downloads for item
+        -k  --min-split-size                Split download of item into multiple ranges
+        --max-overall-download-limit        Limit overall download speed
+
+
+Last updated: Jul 16 2021
---
+++ gogoanime.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+
+import sys
+import re
+import os
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+if not __name__ == '__main__':
+    print("Program must be run by itself. Exiting.")
+    sys.exit(-1)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                         Parameters (TO BE SET BY YOU)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#  Headers and Cookies for streamani/vidstream download page -------------------
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0',
+}
+cookies = {
+    'cf_clearance': 'a8e8039fcefafd6a515195186980c54399b80c4f-1626262802-0-250',
+    #  'prefetchAd_4732994': 'true',
+    #  'prefetchAd_3386133': 'true',
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                    Set-up
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#  Functions -------------------------------------------------------------------
+def getSoup(session, url, headers=headers, cookies={}):
+    response = session.get(url, headers=headers, cookies=cookies)
+    return BeautifulSoup(response.text, "html.parser")
+
+def getDownloadPageHTML(browser, url):
+    """Use selenium to render the download page. This will reveal the download links"""
+    try:
+        timeout = 15
+        browser.get(url)
+        what_is_this = WebDriverWait(browser, timeout).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//body/section" + "/div" * 6 + "/a")
+            )
+        )
+        return BeautifulSoup(browser.page_source, "html.parser")
+    except TimeoutException as error:
+        print("Could not extract HTML for the following site. Timeout reached.")
+        print(url)
+        raise
+
+#  Root URL of gogoanime -------------------------------------------------------
+ROOT_URL = "https://gogoanime.cm"
+
+#  Set up some variables -------------------------------------------------------
+episodes = {
+    'gogoanime' : [],
+    'vidstream' : [],
+    'mp4': [],
+}
+
+s = requests.Session()
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                Anime Selection
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#  Get list of animes to chose from --------------------------------------------
+query = f'{ROOT_URL}/search.html?keyword=' + "+".join(sys.argv[1:])
+soup = getSoup(s, query)
+
+# get list of animes that were found
+anime_list = []
+ul = soup.find('ul', class_='items')
+for lst,rls in zip(ul.find_all('p', class_='name'), ul.find_all('p', class_='released')):
+    link = ROOT_URL + lst.find('a').get('href')
+    name = lst.find('a').get('title')
+    year = re.search(r'\d+', rls.text).group()
+    anime_list.append((name, link, year))
+if not anime_list:
+    print("No anime was found with keyword: '{}'".format(" ".join(sys.argv[1:])))
+    print("Exiting.")
+    sys.exit(1)
+
+# query user to pick an anime
+for i, (name, _, year) in enumerate(anime_list):
+    print("{:5} | {} ({})".format(i,name,year))
+while True:
+    try:
+        choice = int(input("Select anime (enter number): "))
+        if choice < 0:
+            print(">>> Aborting.")
+            sys.exit()
+        elif not choice < len(anime_list):
+            print(">>> Invalid anime specified.")
+            continue
+        break
+    except ValueError:
+        print(">>> Choice must be an integer.")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                          Get GogoAnime Episode links
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+print('Extracting GogoAnime episode links ...')
+
+url, year = anime_list[choice][1:]
+soup = getSoup(s, url)
+
+ep_start = int(soup.find('a', class_='active').get('ep_start'))
+ep_end = int(soup.find('a', class_='active').get('ep_end'))
+
+folder = '{}-{}'.format(re.search(r'[\w-]+$', url).group(), year)
+links_file = f'{folder}/links.txt'
+if os.path.exists(links_file):
+    with open(links_file, 'r') as f:
+        links_file_contents = f.read()
+    # extract existing episode numbers (tightly linked to who file is written/formatted)
+    links_file_episodes = list(map(int, re.findall(r'out=.*ep(\d+).*?\n', links_file_contents)))
+else:
+    links_file_episodes = []
+
+for ep in range(ep_start, ep_end):
+    if ep+1 in links_file_episodes:
+        continue # do not include existing episodes
+    episodes['gogoanime'].append(re.sub(r'category/(.+)', r'\1-episode-'+str(ep+1), url))
+    break ##TODO: REMOVE
+if len(episodes['gogoanime']) == 0:
+    print("No missing episodes to download. Exiting.")
+    sys.exit(0)
+
+#  episodes['gogoanime'] = [
+#      re.sub(r'category/(.+)', r'\1-episode-'+str(ep+1), url)
+#      for ep in range(ep_start, ep_end)
+#      ]
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                  Get streamani/vidstream download page links
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+print('Extracting download page links ...')
+
+# from the yellow download button
+for episode in episodes['gogoanime']:
+    link = getSoup(s, episode).find('li', class_='dowloads').find('a').get('href')
+    name = re.search(r'[\w-]+$', episode).group()
+    # change "episode-x" to "ep00x" (add leading 0s for fixed width numbers)
+    name = re.sub(r'episode-(\d+)$', lambda m: 'ep{:>03}'.format(m.group(1)), name)
+    episodes['vidstream'].append((name,link))
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                    Access download page and get mp4 links
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+print('Extracting video links ...')
+
+print("Setting up selenium FireFox browser ...")
+firefox_options = FirefoxOptions()
+firefox_options.add_argument("--headless")
+firefox_options.add_argument("--private-window")
+
+browser = webdriver.Firefox(options = firefox_options)
+
+# TODO: selenium to execute captcha function => will make download buttons appear
+# https://stackoverflow.com/questions/3232904/using-recaptcha-on-localhost
+for n,episode in episodes['vidstream']:
+    #  soup = getSoup(s, episode, headers=headers, cookies=cookies)
+    print("Extracting {} ...".format(episode))
+    soup = getDownloadPageHTML(browser, episode)
+    """
+    if len(soup.findAll(string=re.compile('captcha', re.IGNORECASE))) > 0:
+        print("Captcha triggered. Exiting.")
+        with open("/tmp/gogoanime.html", 'w') as f:
+            f.write(str(soup))
+        sys.exit(2)
+    """
+    # find link with highest resolution
+    links = soup.find_all('div', class_='dowload')
+    # filter out links without a resolution (i.e. has to contain 360P)
+    links_episodes = [ l for l in links if re.search("[0-9]+P", l.text) ]
+    # sort according to resolution text
+    link_maxres = sorted(links_episodes,
+            key=(lambda l: re.search(r"([0-9]+)P", l.text).group(1))
+            )[0]
+    link = link_maxres.find('a').get('href')
+    resolution = re.search(r"[0-9]+P", link_maxres.text).group()
+    name = f"{n}_{resolution}.mp4"
+    episodes['mp4'].append((name,link,episode))
+
+browser.quit()
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                              Write links to file
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+try:
+    os.mkdir(folder)
+except FileExistsError:
+    pass
+
+with open(links_file, 'a') as f:
+    f.write('\n'.join([ f'{link}\n    out={name}\n    referer={episode}'
+        for name,link,episode in episodes['mp4']]))
+    #  f.write('\n'.join([ f'{link}\n    out={name}\n    referer={episode}'
+    #      for name,link,episode in episodes['mp4']]))
+    print(f"Links written to:\n   {links_file}")
